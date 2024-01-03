54bc1b61f7eed53d1a1a05da33b38ed2c2991eba
Balaji Kosuri <123826865+Balajikosuri@users.noreply.github.com>
2024-01-03 19:23:32 +0530

    "Configured environment variables, connected to MongoDB, and set up express server with user and todo routes."

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
+const connectToDatabase = require("./db/connectToDatabase");
 const mongoose = require("mongoose");
 const express = require("express");
-const bcrypt = require("bcrypt");
 
 const userRoutes = require("./routes/userRoutes");
 const todoRoutes = require("./routes/todoRoutes");
@@ -9,28 +10,6 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.json());
-
-// const mongodbUsername = process.env.MongoDb_Username;
-// const mongodbPassword = process.env.MongoDb_Password;
-const mongodbUsername = "balajikosuri";
-const mongodbPassword = "balajiqwerty123";
-
-function connectToDatabase() {
-  try {
-    mongoose.connect(
-      `mongodb+srv://${mongodbUsername}:${mongodbPassword}@todo1.mxkldpe.mongodb.net/todosAppDb`
-    );
-
-    const db = mongoose.connection;
-
-    db.on("error", console.error.bind(console, "MongoDB connection error:"));
-    db.once("open", () => {
-      console.log("Connected to the database");
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
 
 connectToDatabase();
 
